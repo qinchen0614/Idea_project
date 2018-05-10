38f7683447cf218b053af43fa6b76f46a07d41cc
qinchen <1148768284@qq.com>
2018-05-10 20:51:06 +0800

    java代码

---
+++ src/com/qinchen/demo/ChaiBao.java
@@ -0,0 +1,18 @@
+package com.qinchen.chaibao;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ChaiBao {
+    public static void main(String[] args) throws Exception{
+        URL url = new URL("http://47.92.126.8:8080/gasinternet");
+        URLConnection urlConnection = url.openConnection();
+        InputStream inputStream = urlConnection.getInputStream();
+        int len = 0;
+        byte[] bytes = new byte[1024];
+        while ((len = inputStream.read(bytes))!=-1){
+            System.out.println(new String(bytes,0,len));
+        }
+    }
+}
